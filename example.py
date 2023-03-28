763bc416fa418384de0d7c4bd269c96f5aa0606c
SAAD-3XK <saad.sameer@xloopdigital.com>
2023-03-28 13:56:35 +0500

    Adding new file to repository/Git

---
+++ example.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Person:
+	f_name: str
+	l_naem: str
+	
+	def call_name(self):
+		print(self.f_name + ' ' + self.l_naem)
+
+f=input("Enter first name: ")
+l=input("Enter last name: ")
+my_name=Person(f,l)
+my_name.call_name()
+
